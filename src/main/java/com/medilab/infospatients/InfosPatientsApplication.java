a53196f68fffe0332dbab87b8c1d711f835f4dea
Brault sebastien <braseb37@gmail.com>
2026-02-07 09:36:56 +0100

    init the project

---
+++ src/main/java/com/medilab/infospatients/InfosPatientsApplication.java
@@ -0,0 +1,13 @@
+package com.medilab.infospatients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosPatientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfosPatientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medilab/infospatients/InfosPatientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilab.infospatients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosPatientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
