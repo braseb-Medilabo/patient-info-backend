92e2766e00d559886f09fd760ba782185dc0d519
Brault sebastien <braseb37@gmail.com>
2026-04-05 07:34:34 +0200

    Update spring boot version and append a global exceptionHandler

---
+++ src/main/java/com/medilab/infospatients/exception/GlobalExceptionHandler.java
@@ -0,0 +1,35 @@
+package com.medilab.infospatients.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidation(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        
+        ex.getBindingResult().getFieldErrors()
+                               .forEach(err-> errors.put(err.getField(), err.getDefaultMessage()));
+        
+        
+        return ResponseEntity.badRequest().body(Map.of(
+                "message", "validation failed",
+                "errors", errors));
+    }
+    
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handlerException(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(Map.of("message", ex.getMessage()));
+    }
+    
+    
+}
