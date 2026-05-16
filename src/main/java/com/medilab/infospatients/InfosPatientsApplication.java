4cc66afc65b466cbbcdd59f1051a47cc21726868
Brault sebastien <braseb37@gmail.com>
2026-05-16 16:09:08 +0200

    Append swagger gateway configuration

--- src/main/java/com/medilab/infospatients/InfosPatientsApplication.java
+++ src/main/java/com/medilab/infospatients/InfosPatientsApplication.java
@@ -3,11 +3,14 @@ package com.medilab.infospatients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InfosPatientsApplication {
-
-	public static void main(String[] args) {
+    
+    public static void main(String[] args) {
 		SpringApplication.run(InfosPatientsApplication.class, args);
 	}
+	
+	
 
 }
