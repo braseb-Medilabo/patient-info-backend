0d9a1bfb6b8f673ff4b7d97f342fd7b966c0e1b9
Brault sebastien <braseb37@gmail.com>
2026-06-06 14:29:42 +0200

    Change the path of the swagger api-doc (json) and append the bearer token on the swagger doc

--- src/main/java/com/medilab/infospatients/configuration/OpenApiCustomConfiguration.java
+++ src/main/java/com/medilab/infospatients/configuration/OpenApiCustomConfiguration.java
@@ -6,8 +6,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.servers.Server;
+
+@OpenAPIDefinition(info = @Info(title = "Patient API",
+                                version = "1.0"),
+                    security = @SecurityRequirement(name = "bearerAuth"))
 
 @Configuration
 public class OpenApiCustomConfiguration {
---
+++ src/main/java/com/medilab/infospatients/configuration/SwaggerConfig.java
@@ -0,0 +1,15 @@
+package com.medilab.infospatients.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+
+@Configuration
+@SecurityScheme(name = "bearerAuth",
+                type = SecuritySchemeType.HTTP,
+                scheme = "bearer",
+                bearerFormat = "JWT")   
+public class SwaggerConfig {
+
+}
