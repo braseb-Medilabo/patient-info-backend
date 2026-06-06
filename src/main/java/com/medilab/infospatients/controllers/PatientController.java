e56711c349ab1aea35e4becf335170b8c7cfd610
Brault sebastien <braseb37@gmail.com>
2026-06-06 14:29:42 +0200

    Change the path of the swagger api-doc (json) and append the bearer token on the swagger doc

--- src/main/java/com/medilab/infospatients/controllers/PatientController.java
+++ src/main/java/com/medilab/infospatients/controllers/PatientController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.medilab.infospatients.entitys.Patient;
 import com.medilab.infospatients.services.PatientService;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 
 @RestController
@@ -24,6 +25,7 @@ public class PatientController {
     public PatientController(PatientService patientService) {
         this.patientService = patientService;
     }
+    
     
     @GetMapping("/patient/list")
     public ResponseEntity<Iterable<Patient>> getPatients() {
